ece98d38dd39f91013efa6c3e9cc29db39d5e1a4
Pavan S <pavangatt@github.com>
2026-07-02 15:00:36 +0530

    changed the vite file

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
   server: {
     port: 5173,
   },
+  build: {
+    outDir: './', // This directs the build output to the root folder instead of /dist
+    emptyOutDir: false, // CRUCIAL: Prevents Vite from deleting your source files (like src/ or package.json) when rebuilding
+  },
 });
